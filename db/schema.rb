d38e05892457b1b032f34f07e97d4c160918dfe4
Jeff Thomas <subskriptions@gmail.com>
2014-10-21 16:44:35 -0600

    Add seeds file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021205508) do
+ActiveRecord::Schema.define(version: 20141021223431) do
 
   create_table "rooms", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20141021205508) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Room.create!(name: "Living Room", private: false)
+Room.create!(name: "Kitchen", private: false)
+Room.create!(name: "Bathroom", private: false)
+Room.create!(name: "Bedroom 1", private: true)
+Room.create!(name: "Bedroom 2", private: true)
+Room.create!(name: "Master Bedroom", private: true)
+Room.create!(name: "Wine Cellar", private: true)
+
+User.create!( email: "guest@house.com", password: "password", role: 1)
+User.create!( email: "owner@house.com", password: "password", role: 0 )
+
+puts "#{Room.count} rooms added."
+puts "Owner account created: owner@house.com:password"
+puts "Visitor account created: visitor@house.com:password"
+
