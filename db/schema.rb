f39bbc8fe4a6d7d088862e0779ceda5280734b88
Jeff Thomas <subskriptions@gmail.com>
2014-10-21 16:23:04 -0600

    Add user signup/login/logout links.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021204252) do
+ActiveRecord::Schema.define(version: 20141021205508) do
+
+  create_table "rooms", force: true do |t|
+    t.string   "name"
+    t.boolean  "private"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
